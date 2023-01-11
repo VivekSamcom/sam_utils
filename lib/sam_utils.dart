9922212bb4569d6d165bf9b11f130662b773cd58
Punit <punitsamcom84@gamil.com>
2023-01-11 14:51:11 +0530

    init

--- lib/sam_utils.dart
+++ lib/sam_utils.dart
@@ -1,7 +1,38 @@
 library sam_utils;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+import 'log_type_enum.dart';
+
+const bool isPrintLog = true;
+
+printLog(String strLogMessage, [LogTypeEnum? notificationType, String? strTag]) {
+  if (isPrintLog && strLogMessage.isNotEmpty) {
+    if (notificationType != null) {
+      if (notificationType == LogTypeEnum.TYPE_ERROR) {
+        debugPrint("----------> ERROR LOG${_getTagMessage(strTag)} <----------");
+        debugPrint("| ###> $strLogMessage");
+      } else if (notificationType == LogTypeEnum.TYPE_NULL) {
+        debugPrint("----------> LOG NULL${_getTagMessage(strTag)} <----------");
+        debugPrint("| ???> $strLogMessage");
+      } else if (notificationType == LogTypeEnum.TYPE_SIMPLE) {
+        debugPrint("----------> LOG${_getTagMessage(strTag)} <----------");
+        debugPrint("| ^^^> $strLogMessage");
+      } else if (notificationType == LogTypeEnum.TYPE_WITH_TAG) {
+        debugPrint("----------> LOG${_getTagMessage(strTag)} <----------");
+        debugPrint("| $strLogMessage");
+      } else {
+        debugPrint("| $strLogMessage");
+      }
+    } else {
+      debugPrint("| $strLogMessage");
+    }
+  }
+}
+
+String _getTagMessage(String? strTag) {
+  if (strTag != null && strTag.isNotEmpty) {
+    return " : ( $strTag )";
+  }
+  return "";
 }
